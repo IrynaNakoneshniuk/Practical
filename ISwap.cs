99ad7011e2e88c8705891dd3a311465477ca3f53
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2022-07-20 00:18:56 +0300

    Add files via upload

---
+++ ISwap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Random;
+
+namespace H_W._25._07
+{
+    internal  interface ISwap<T>
+    {
+        void Swap(T firstV,  T secondV);
+    }
+    internal class TestArray : ISwap<int>
+    {
+        public int[] TestArr;
+        public int? Length;
+        public TestArray(int Length)
+        {
+            this.Length = Length;
+            TestArr = new int[Length];
+        }
+        public void FillRandom(int Max)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < TestArr.Length; i++)
+            {
+                TestArr[i] = rnd.Next(Max);
+            }
+        }
+        public void Output()
+        {
+            foreach (int i in TestArr)
+            {
+                Console.Write($" {i}");
+            }
+        }
+        public void Swap(int index1,  int index2)
+        {
+            int Temp = this.TestArr[index1];
+            this.TestArr[index1] = this.TestArr[index2];
+            this.TestArr[index2] = Temp;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using static System.Console;
+using static System.Random;
+
+namespace H_W._25._07
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            try
+            {
+                TestArray array = new TestArray(5);
+                array.FillRandom(10);
+                array.Output();
+                WriteLine();
+                WriteLine("--------Array after swap first and last position--------");
+                array.Swap(1, 4);
+                array.Output();
+            }catch(Exception ex)
+            {
+                WriteLine(ex.Message.ToString());   
+            }
+        }
+        static void Task2()
+        {
+            try
+            {
+                Write("Queue: ");
+                Random random = new Random();
+                Queue<int> queue = new Queue<int>();
+                for (int i = 0; i < 5; i++)
+                {
+                    queue.Push(i, i);
+                }
+                queue.Print();
+                WriteLine();
+                WriteLine($"Peek: {queue.Peek()}");
+                queue.Pop();
+                queue.Print();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        static void Task3()
+        {
+            try
+            {
+                Write("Queue: ");
+                Random random = new Random();
+                QueueRing <int> queue = new QueueRing<int>(5);
+                for(int i = 0; i < 5; i++)
+                {
+                    queue.Push(i);
+                }
+                queue.Print();
+                queue.Move();
+                WriteLine("Queue after moving: ");
+                queue.Print();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Task1();
+            //Task2();
+            Task3();
+        }
+    }
+}
