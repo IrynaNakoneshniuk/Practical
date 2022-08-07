9881150d5be7476a5c13b8f57952e36e12fb0037
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2022-08-07 22:40:21 +0300

    Add files via upload

---
+++ IEmployee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace H_W._28._07
+{
+    internal interface IEmployee
+    {
+        string? Name { get; set; }
+        int? Age { get; set; }
+        int? YearsExperience { get; set; }
+        float? Salary { get; set; }
+        string? Activity { get; set; }
+        void ShowEmployeeInfo();
+    }
+    internal class Waiter : IEmployee
+    {
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public int? YearsExperience { get; set; }
+        public float? Salary { get; set; }
+        public string? Activity { get; set; }
+        public Waiter(string? name, int? age, int? yearsExperience, float? salary, string? activity)
+        {
+            Name = name;
+            Age = age;
+            YearsExperience = yearsExperience;
+            Salary = salary;
+            Activity = activity;
+        }
+        public Waiter()
+        {
+            Name = null;
+            Age = null;
+            YearsExperience = null;
+            Salary = null;
+            Activity = null;
+        }
+        public void ShowEmployeeInfo()
+        {
+            WriteLine($"Name: {Name}, Age: {Age},Experience: {YearsExperience},Activity: {Activity},Salary: {Salary}");
+        }
+    }
+    internal class Cashier: Waiter
+    {
+        public Cashier(string? name, int? age, int? yearsExperience, float? salary, string? activity)
+        {
+            Name = name;
+            Age = age;
+            YearsExperience = yearsExperience;
+            Salary = salary;
+            Activity = activity;
+        }
+    }
+    internal class Manager : Waiter
+    {
+        public Manager(string? name, int? age, int? yearsExperience, float? salary, string? activity)
+        {
+            Name = name;
+            Age = age;
+            YearsExperience = yearsExperience;
+            Salary = salary;
+            Activity = activity;
+        }
+    }
+}
---
+++ Oceanarium.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H_W._28._07
+{
+    internal class Oceanarium : IEnumerable
+    {
+        private IOceanDwellers[] _litsDwellers;
+        public Oceanarium(int length)
+        {
+            _litsDwellers = new IOceanDwellers[length];
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+        public EnumOceanarium GetEnumerator()
+        {
+            return new EnumOceanarium(_litsDwellers);
+        }
+    }
+    internal class EnumOceanarium : IEnumerator
+    {
+        public IOceanDwellers[] List;
+        public int position = -1;
+
+        public EnumOceanarium(IOceanDwellers[] list)
+        {
+            List = list;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < List.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+        public IOceanDwellers Current
+        {
+            get
+            {
+                try
+                {
+                    return List[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
+
